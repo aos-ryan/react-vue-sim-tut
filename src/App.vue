cee4ddb29a99cce6d50d566fedeb8e11c3b8d3c6
ryanallenk <ryanallenk@gmail.com>
2022-11-28 16:17:45 -0500

    create first basic component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const useChangeWithDelay = function(state, newVal, delay) {
+  setTimeout(() => {
+    state.value = newVal
+  }, delay)
+}
+const product = ref('Socks');
+
+useChangeWithDelay(product, 'New Socks', 1000)
+
+</script>
+
+<template>
+  <h1>{{ product }}</h1>
+</template>
