50543e165578edf2e4ef31024d549dd595891d81
ryanallenk <ryanallenk@gmail.com>
2022-11-29 09:49:59 -0500

    add basic event handling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <script setup>
 import { ref } from 'vue';
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 // example of a react custom "hook" done in vue
 const useChangeWithDelay = function(state, newVal, delay) {
   setTimeout(() => {
     state.value = newVal
   }, delay)
-}
+};
 const product = ref('Socks');
-const image = ref(socksGreenImage)
-const inStock = ref(true)
+const image = ref(socksGreenImage);
+const inStock = ref(true);
 
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 const variants = ref([
-  { id: 2234, colour: 'green'},
-  { id: 2235, colour: 'blue'}
-])
+  { id: 2234, colour: 'green', image: socksGreenImage},
+  { id: 2235, colour: 'blue', image: socksBlueImage}
+]);
+
+const cart = ref(0);
+const addToCart = () => cart.value += 1
+
+const updateImage = (variantImage) => {
+  image.value = variantImage
+}
 </script>
 
 <template>
   <div class="nav-bar"></div>
+  <div class="cart">Cart ({{ cart }})</div>
     <div class="product-display">
       <div class="product-container">
         <div class="product-image">
@@ -34,8 +43,13 @@ const variants = ref([
           <ul>
             <li v-for="detail in details">{{ detail }}</li>
           </ul>
-          <div v-for="variant in variants" :key="variant.id">{{ variant.colour }}</div>
+          <div 
+          v-for="variant in variants" 
+          :key="variant.id" 
+          @mouseover="updateImage(variant.image)">
+          {{ variant.colour }}</div>
         </div>
+        <button class="button" v-on:click="addToCart">Add to Cart</button>
       </div>
     </div>
 </template>
